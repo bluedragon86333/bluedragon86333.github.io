a70c7655d49ef4ad8f26bb5c00ac86d538e3f5f3
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2023-01-31 07:56:38 -0500

    Update caesar_script.js

--- projects/caesar/caesar_script.js
+++ projects/caesar/caesar_script.js
@@ -1,13 +1,15 @@
-const alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 document.getElementById("output").innerHTML = "booted JavaScript successfully";
 
 function generatePossibilities(original) {
-	let encrypted = original;
+	const alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	let encrypted = original.inputbox.value;
 	let decrypted = "";
-	for (let offset = 0; offset < 25; offset++) {
+	for (let offset = 0; offset < 26; offset++) {
+		decrypted += offset + ": ";
 		for (let i = 0; i < encrypted.length;i++) {
 			decrypted += alphabet[alphabet.indexOf(encrypted[i]) + offset];	
 		}
+		decrypted += " ";
 	}
 	document.getElementById("output").innerHTML = decrypted;
 }
