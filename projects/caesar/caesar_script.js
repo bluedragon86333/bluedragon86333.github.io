28eea6240b2f59f04e22fff190a3f860522f2742
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2023-02-01 10:33:17 -0500

    Update caesar_script.js

--- projects/caesar/caesar_script.js
+++ projects/caesar/caesar_script.js
@@ -3,25 +3,29 @@ document.getElementById("output").innerHTML = "booted JavaScript successfully";
 function addItems(content,contentList) { //prints results
 	document.getElementById("output").innerHTML = content;
 	
-	
-	const row = document.createElement("tr");
-	const node = document.createTextNode("");
-	row.appendChild(node);
-	const element = document.getElementById("div1");	
+	for (let i=0;i<contentList.length,i++) {
+		const row = document.createElement("tr");
+		const node = document.createTextNode(content);
+		row.appendChild(node);
+		const element = document.getElementById("div1");
+	}
 }
 
 
 function generatePossibilities(original) {
 	const alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-	let encrypted = original.inputbox.value;
+	let encrypted = original.inputbox.value.toLowerCase();
 	let decrypted = "";
+	let decryptedList = [];
 	for (let offset = 0; offset < 26; offset++) {
 		decrypted += offset + ": ";
+		decryptedList.append("");
 		for (let i = 0; i < encrypted.length;i++) {
-			decrypted += alphabet[alphabet.indexOf(encrypted[i]) + offset];	
+			decrypted += alphabet[alphabet.indexOf(encrypted[i]) + offset];
+			decryptedList[decryptedList.length - 1] += alphabet[alphabet.indexOf(encrypted[i]) + offset];
 		}
 		decrypted += " ";
 	}
-	addItems(decrypted);
+	addItems(decrypted,decryptedList);
 	
 }
