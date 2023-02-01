2f2b6a5686e6a60236b183359052250ef6d0d150
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2023-02-01 07:51:12 -0500

    Update caesar_script.js

--- projects/caesar/caesar_script.js
+++ projects/caesar/caesar_script.js
@@ -1,5 +1,16 @@
 document.getElementById("output").innerHTML = "booted JavaScript successfully";
 
+function addItems(content,contentList) { //prints results
+	document.getElementById("output").innerHTML = content;
+	
+	
+	const row = document.createElement("tr");
+	const node = document.createTextNode("");
+	row.appendChild(node);
+	const element = document.getElementById("div1");	
+}
+
+
 function generatePossibilities(original) {
 	const alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 	let encrypted = original.inputbox.value;
@@ -11,5 +22,6 @@ function generatePossibilities(original) {
 		}
 		decrypted += " ";
 	}
-	document.getElementById("output").innerHTML = decrypted;
+	addItems(decrypted);
+	
 }
