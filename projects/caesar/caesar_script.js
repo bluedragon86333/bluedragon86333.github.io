82a3c8c2f72742ce570dd038b6ab0e589b497a05
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2023-02-03 10:31:38 -0500

    Update caesar_script.js

--- projects/caesar/caesar_script.js
+++ projects/caesar/caesar_script.js
@@ -16,7 +16,8 @@ function addItems(content,contentList) { //prints results
 
 function generatePossibilities(original) {
 	const alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-	let encrypted = original.inputbox.value.toLowerCase();
+	let encrypted = original.inputbox.value;
+	
 	let decrypted = "";
 	let decryptedList = [];
 	for (let offset = 0; offset < 26; offset++) {
