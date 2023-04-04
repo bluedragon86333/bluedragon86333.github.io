962b46b3a2c2376426aaa210d1ac5d6364da56d6
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2023-04-04 01:01:44 -0400

    Update mouse.js

--- projects/cafe-game/mouse.js
+++ projects/cafe-game/mouse.js
@@ -1,17 +1,27 @@
 //set up the canvas and context
 //var canvas = document.getElementById("game-canvas");
-
+var mouseDown = false;
 //report the mouse position on click
 canvas.addEventListener("mousedown", function (evt) {
     var mousePos = getMousePos(canvas, evt);
+	mouseDown = true;
     document.getElementById("mouseCoords").innerHTML = mousePos.x + ',' + mousePos.y;
+	
+}, false);
+
+canvas.addEventListener("mouseup", function (evt) {
+	mouseDown = false;
 }, false);
 
 //Get Mouse Position
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
+        x: Math.round((evt.clientX - rect.left) / 2),
+        y: Math.round((evt.clientY - rect.top) / 2)
     };
 }
+
+var updatemouse = setInterval(function() {
+	document.getElementById("mouseDown").innerHTML = "mouseDown = " + mouseDown;
+},30);
