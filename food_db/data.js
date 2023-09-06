a8fa51a5210b78d82814614a1c17dbf29053df4c
bluedragon86333 <94876427+bluedragon86333@users.noreply.github.com>
2023-09-06 13:57:30 -0400

    Update data.js

--- food_db/data.js
+++ food_db/data.js
@@ -1,17 +1,17 @@
 var list = [
 	{
-		"name":"",
-		"source":"",
-		"country":"",
-		"protein":"",
-		"carb":""
+		"name":"Chicken Adobo",
+		"source":"Cook's Illustrated",
+		"country":"Phillipines",
+		"protein":"chicken",
+		"carb":"rice"
 	},
 	{
-		"name":"",
-		"source":"",
-		"country":"",
-		"protein":"",
-		"carb":""
+		"name":"Sushi",
+		"source":"brain",
+		"country":"Japan",
+		"protein":"fish",
+		"carb":"rice"
 	},
 	{
 		"name":"",
